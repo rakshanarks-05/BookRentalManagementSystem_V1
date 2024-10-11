49e80b95969bf6741daa934703359e716491d8ec
UT01183 <UT01183@UNICOMTIC73>
2024-10-11 14:17:07 +0530

    Add project files.

---
+++ BookRentalManagementSystem/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRentalManagementSystem
+{
+    public  class Book
+    {
+        private string Bookid {  get; set; }
+        private string Title { get; set; }
+        private string Author { get; set; }
+        private decimal RentalPrice { get; set; }
+    }
+}
